d70ab4be834f13ebef541bbcb16fa0431267fb39
Caio Gustavo Rodrigues da Silva <silva.caiosfc@hotmail.com>
2024-10-01 21:25:45 -0300

    Create Configurations

---
+++ src/Blog_DevIO.Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Blog_DevIO.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_DevIO.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(p => p.LastName).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(p => p.NickName).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(p => p.Age).IsRequired();
+            builder.Property(p => p.Creation).HasDefaultValueSql("getdate()").ValueGeneratedOnAdd();
+
+            builder.HasMany(p => p.Posts)
+            .WithOne(p => p.User)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(p => p.Comments)
+             .WithOne(p => p.User)
+             .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
